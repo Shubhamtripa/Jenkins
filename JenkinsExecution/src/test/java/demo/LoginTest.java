17abb44d54a67655224d908dca05663b5d76f966
Shubhamtripa <shubhamtripathi6930@gmail.com>
2024-09-17 20:56:13 +0530

    2nd push

---
+++ JenkinsExecution/src/test/java/demo/LoginTest.java
@@ -0,0 +1,12 @@
+package demo;
+import org.testng.annotations.Test;
+
+import Generic_Utilities.BaseClass;
+
+public class LoginTest extends BaseClass {
+	
+	@Test
+	public void loginIntoApp() {
+		System.out.println("Login successfull!!!");
+	}
+}
